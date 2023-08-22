9d80285a6c0db864ff2966994fd2df3cea2413f5
dongqingruan <zhangguo8@foxmail.com>
2023-08-22 13:52:55 +0800

    移出 vue-element-admin

--- frontend/demos/frameworks/vue-element-admin/src/api/personnel.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getPersonnel(query) {
-  return request({
-    url: '/vue-element-admin/article/list',
-    method: 'get',
-    params: query
-  })
-}
